e54945222438e57b7d8139357ee7305d72f94145
Hajas András <bandi.14@hotmail.com>
2020-02-19 10:43:09 +0100

    Test
    Heroku

---
+++ Easylearner/src/main/kotlin/hu/onlab/Easylearner/EasylearnerApplication.kt
@@ -0,0 +1,11 @@
+package hu.onlab.Easylearner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EasylearnerApplication
+
+fun main(args: Array<String>) {
+	runApplication<EasylearnerApplication>(*args)
+}
---
+++ Easylearner/src/test/kotlin/hu/onlab/Easylearner/EasylearnerApplicationTests.kt
@@ -0,0 +1,13 @@
+package hu.onlab.Easylearner
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EasylearnerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
